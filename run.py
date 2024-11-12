fc31e5a38da595936625be27052b060c1684a5b2
NewBieCoderXD <fruity5555@gmail.com>
2024-11-12 11:32:50 +0700

    use relpath

--- run.py
+++ run.py
@@ -2,4 +2,6 @@ import sys
 import os 
 import subprocess
 dirname = os.path.dirname(os.path.realpath(__file__))
-subprocess.run(['python','-m',sys.argv[1].split(dirname)[1].replace('/','.').removesuffix(".py").removeprefix(".")])
+target_path = sys.argv[1]
+target_path = os.path.relpath(target_path,dirname)
+subprocess.run(['python','-m',target_path.replace('/','.').removesuffix(".py").removeprefix(".")])
