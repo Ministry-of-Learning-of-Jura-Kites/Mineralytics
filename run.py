c6b5cd592f21c7ecbab1a2ac74b4af37cb52030f
SigmaFong <ahfong2548@gmail.com>
2024-11-12 11:33:59 +0700

    use os.path.sep

--- run.py
+++ run.py
@@ -4,4 +4,4 @@ import subprocess
 dirname = os.path.dirname(os.path.realpath(__file__))
 target_path = sys.argv[1]
 target_path = os.path.relpath(target_path,dirname)
-subprocess.run(['python','-m',target_path.replace('/','.').removesuffix(".py").removeprefix(".")])
+subprocess.run(['python','-m',target_path.replace(os.path.sep,'.').removesuffix(".py").removeprefix(".")])
