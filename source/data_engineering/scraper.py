0a837dd37441daa263cf10f1b2be4bef272aa953
NewBieCoderXD <fruity5555@gmail.com>
2024-11-17 21:13:31 +0700

    add scraper

---
+++ source/data_engineering/scraper.py
@@ -0,0 +1,8 @@
+import bs4 as beatifulsoup
+
+def scrape_paper(url):
+  # from academic.oup.com
+  title = 
+  pass
+
+scrape_paper("https://academic.oup.com/biomet/article-abstract/45/1-2/229/264691?login=false")
