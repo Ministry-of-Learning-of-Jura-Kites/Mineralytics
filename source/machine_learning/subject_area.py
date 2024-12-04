f35d196fbdfd5a18c72725657557a888dbf8b89f
Sirawit Viriyaphisathan <sirawit@SorawitWork.local>
2024-12-04 15:55:57 +0700

    get machine learning set up

---
+++ source/machine_learning/subject_area.py
@@ -0,0 +1,49 @@
+import json
+import pandas as pd
+import source.base_api as base_api
+import matplotlib.pyplot as plt
+import streamlit as st
+
+lang_column = "abstracts-retrieval-response.subject-areas.subject-area"
+
+
+def transform(file):
+    data = json.load(file)
+    data = data["abstracts-retrieval-response"]["subject-areas"]["subject-area"]
+    df = pd.json_normalize(data)
+    # print(df.columns)
+    # print(df.columns)
+    return df
+    
+
+# if __name__ == "__main__":
+# @st.cache_data
+def get_data():
+    return base_api.load_all_data(transform)
+
+def language():
+    df = get_data()
+    # for testing
+    # df = base_api.load_data_of_year(
+    #     2018,
+    #     lambda df: df.drop(
+    #         columns=df.columns.difference(
+    #             ["abstracts-retrieval-response.language.@xml:lang"]
+    #         )
+    #     ),
+    #     100,
+    # )
+
+    value_counts = df[lang_column].value_counts()
+    fig, ax = plt.subplots()
+    ax.bar(
+        value_counts.index.to_list(),
+        value_counts.values,
+    )
+    ax.set_yscale("log", base=2)
+    ax.set_ylabel("count")
+    ax.set_xlabel("subject-area")
+    return fig
+
+data = get_data()
+print(data[["@abbrev" , "$"]]) 
---
+++ source/machine_learning/training.py
@@ -0,0 +1,6 @@
+import source.machine_learning.subject_area as sa
+# import 
+
+data = sa.get_data()
+
+print(data)
