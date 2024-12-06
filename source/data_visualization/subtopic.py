5f242a8919da7668bdefc688e190f59bc7a0d84a
Oatkrub <sirawit.v@gmail.com>
2024-12-06 19:21:49 +0700

    show graph prediction

--- source/data_visualization/subtopic.py
+++ source/data_visualization/subtopic.py
@@ -1,8 +1,9 @@
+import pandas as pd
 from data_visualization.get_data import get_data
 import plotly.express as px
 
 
-def get_fig(subtopic: str):
+def get_fig(subtopic: str , model=None):
     data = get_data()[["subtopic","year"]]
 
     data = data[data["subtopic"] == subtopic]
@@ -14,10 +15,21 @@ def get_fig(subtopic: str):
         .sort_values(by="year", ascending=True)
     )
     data.dropna(subset="year", inplace=True)
-
+    if model :
+      
+      predicted_data = pd.DataFrame({
+          "subtopic": [subtopic, subtopic],
+          "year": [2024, 2025],
+          "count_subtopic": [
+          model.predict([[2024]])[0],
+          model.predict([[2025]])[0], 
+          ],
+      })
+      data = pd.concat([data, predicted_data])
+    #create bar
     fig = px.bar(data,"year","count_subtopic",labels={
       "year":"Year",
       "count_subtopic":"Count of {}".format(subtopic)
     })
-
+    fig.update_traces(marker_color=data['year'].apply(lambda x: 'green' if x in [2024, 2025] else 'blue'))
     return fig
--- source/website.py
+++ source/website.py
@@ -8,9 +8,26 @@ import data_visualization.openaccess as openaccess
 import data_visualization.city as city
 import data_visualization.subtopic as subtopic
 from data_visualization.get_data import get_data
+import os
+import pickle
+
 st.title('Mineralytics')
 
+def load_pickle(path_to_pickle , file_name) :
+  absolute_path = base_api.relative_to_abs(path_to_pickle)
+  if file_name.endswith('.pkl'):
+      file_path = os.path.join(absolute_path, file_name)
+      try:
+          with open(file_path, 'rb') as file:
+              load_object = pickle.load(file)
+          return load_object
+      except FileNotFoundError:
+            return None 
+  return None
+  
+
 if __name__=="__main__":
+  
   st.header("langauage")
   language_fig = language.get_fig()
   st.pyplot(language_fig)
@@ -30,5 +47,18 @@ if __name__=="__main__":
       index=None,
       placeholder="Select subtopic",
   )
-  subtopic_fig = subtopic.get_fig(selected_subtopic)
-  st.plotly_chart(subtopic_fig)
+  if selected_subtopic:
+    subtopic_code = get_data().loc[get_data()["subtopic"] == selected_subtopic, "subtopic_code"].values[0]
+    choose = "Subtopic_" + subtopic_code + ".pkl"
+    model = load_pickle(
+            ["source", "machine_learning", "model", "subtopic"],
+            choose
+        )
+        
+    if model:  
+        subtopic_fig = subtopic.get_fig(selected_subtopic, model)
+        st.plotly_chart(subtopic_fig)
+    else:
+        subtopic_fig = subtopic.get_fig(selected_subtopic)
+        st.plotly_chart(subtopic_fig)
+    
