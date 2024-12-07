75e76b3ea5784a780653779c51b14963be9a9b8f
NewBieCoderXD <fruity5555@gmail.com>
2024-12-07 13:36:01 +0700

    setup docker for scraper

--- source/data_engineering/paper_scraper/main.py
+++ source/data_engineering/paper_scraper/main.py
@@ -14,22 +14,22 @@ def scrape_paper(url):
     soup = BeautifulSoup(html_page, "html.parser")
 
     title = next(soup.select_one(".c-article-title").children)
-    # print(title)
+    print(title)
 
     authors_node = map(
         get_first_child,
         soup.select("li.c-article-author-list__item a[data-test='author-name']"),
     )
-    # print(list(authors_node))
+    print(list(authors_node))
 
     published_date = soup.select_one(
         "li.c-article-identifiers__item:nth-child(2) time"
     ).decode_contents()
     published_date = datetime.strptime(published_date, "%d %B %Y").date()
-    # print(published_date)
+    print(published_date)
 
     references = map(get_first_child, soup.select("p.c-article-references__text"))
-    # print(list(references))
+    print(list(references))
 
     organizations = map(
         get_first_child,
@@ -42,18 +42,18 @@ def scrape_paper(url):
         get_first_child,
         soup.select("ol.c-article-author-affiliation__list li p:nth-child(2)"),
     )
-    #print(list(zip(organizations, organizations_authors)))
+    print(list(zip(organizations, organizations_authors)))
     
     journal_title = map(
         get_first_child,
         soup.select("i[data-test='journal-title']"),
     )
-    #print(list(journal_title))
+    print(list(journal_title))
     
     subject = map(
          get_first_child,
          soup.select("li.c-article-subject-list__subject"))
-    #print(list(subject))
+    print(list(subject))
 
 
-scrape_paper("https://www.nature.com/articles/nphys2615")
+scrape_paper("https://www.nature.com/articles/s41567-024-02648-0")
